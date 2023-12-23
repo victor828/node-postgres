da9500997263cb2bde97bee19a3079e57e7bc35a
Víctor Orozco <veom104@gmail.com>
2023-12-23 12:46:31 -0800

    adde expres.json

--- build/index.js
+++ build/index.js
@@ -6,5 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes/routes"));
 const app = (0, express_1.default)();
+const port = 5000;
+app.use(express_1.default.json());
 app.use(routes_1.default);
-app.listen(5000);
+app.listen(port, () => console.log("Conectado"));
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import route from "./routes/routes";
+import fs from "node:fs";
 
 const app = express();
-const port = 5500;
-
+const port = 5000;
+app.use(express.json());
 app.use(route);
-app.listen(port);
+app.listen(port, () => console.log("Conectado"));
